846add6dd2cecb5ec8b04b6d460c7043fd641f16
christophdebaene <christoph.debaene@gmail.com>
2015-08-09 15:39:53 +0200

    DiscogsClientFixture missing

---
+++ src/DiscogsConnect.Test/DiscogsClientFixture.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace DiscogsConnect.Test
+{
+    public class DiscogsClientFixture
+    {
+        const string UserAgent = "DiscogsConnect/2.0";
+        const string Key = "<fill in>";
+        const string Secret = "<fill in>";
+        
+        public IDiscogsClient DiscogsClient { get; private set; }
+
+        public DiscogsClientFixture()
+        {
+            DiscogsClient = new DiscogsClient(UserAgent, Key, Secret);            
+        }
+    }
+
+    [CollectionDefinition("DiscogsClient")]
+    public class DiscogsClientCollection : ICollectionFixture<DiscogsClientFixture>
+    {
+    }
+}
